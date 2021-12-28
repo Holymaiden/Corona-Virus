8df9f563fd58964be9d497198650c05e54694210
Fihaa <haikalwatampone3@gmail.com>
2021-12-28 16:43:41 +0800

    add api corona in index.php

--- api.php
+++ api.php
@@ -6,5 +6,6 @@ function get_api_indo()
         global $url_api;
         $api = $url_api . '/indonesia';
         $json = @file_get_contents($api);
+        $json = json_decode($json, true);
         return ($json);
 }
