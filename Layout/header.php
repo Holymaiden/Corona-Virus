e2af688988803b5ce5b016958c1c02b58cad63c9
Fihaa <haikalwatampone3@gmail.com>
2021-12-28 17:39:08 +0800

    create Header and Footer for Layout

---
+++ Layout/header.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+        <!-- google fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Roboto:100,100i,300,300i,400,400i,500,500i,700,700i,900,900i&display=swap" rel="stylesheet">
+        <link rel="shortcut icon" type="image/x-icon" href="assets/images/x-icon/01.png">
+
+        <link rel="stylesheet" href="assets/css/animate.css">
+        <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+        <link rel="stylesheet" href="assets/css/icofont.min.css">
+        <link rel="stylesheet" href="assets/css/lightcase.css">
+        <link rel="stylesheet" href="assets/css/swiper.min.css">
+        <link rel="stylesheet" href="assets/css/style.css">
+
+        <title><?= $title ?></title>
+</head>
